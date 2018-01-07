181bb2b6596123d1abd9d35eddff4542b8b8a457
Ungaminga <loljkpro@cock.li>
2018-01-07 18:24:39 +0200

    [2] Реализация получения списка видео с ютуба.

--- youtube_vk_poster.py
+++ youtube_vk_poster.py
@@ -6,12 +6,42 @@ Created on Sun Jan  7 17:48:39 2018
 """
 
 import configparser
+Config = configparser.ConfigParser()
+
+import urllib
+import json
+def get_all_video_in_channel():
+    api_key = Config['YouTube']['ApiKey']
+    channel_id = Config['YouTube']['ChannelId']
+
+    base_video_url = 'https://www.youtube.com/watch?v='
+    base_search_url = 'https://www.googleapis.com/youtube/v3/search?'
+
+    first_url = base_search_url+'key={}&channelId={}&part=snippet,id&order=date&maxResults=50'.format(api_key, channel_id)
+
+    video_links = []
+    url = first_url
+    while True:
+        inp = urllib.request.urlopen(url)
+        resp = json.load(inp)
+
+        for i in resp['items']:
+            if i['id']['kind'] == "youtube#video":
+                video_links.append(base_video_url + i['id']['videoId'])
+
+        try:
+            next_page_token = resp['nextPageToken']
+            url = first_url + '&pageToken={}'.format(next_page_token)
+        except:
+            break
+    return video_links
+
 def main():
-    Config = configparser.ConfigParser()
     Config.read("conf.ini")
     if (Config.sections() != ['YouTube', 'Vk']):
         print("Wrong Configs")
         return
-    
+    print (get_all_video_in_channel())
+
 if __name__ == "__main__":
     main()
